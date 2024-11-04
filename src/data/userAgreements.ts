2f78e3aae71c91a967ce1bd252cae7a9adf3f020
Abraham Masresha <abrehammasresha890@gmail.com>
2024-11-04 20:07:41 +0300

    created interface for userAgreements

--- src/data/userAgreements.ts
+++ src/data/userAgreements.ts
@@ -3,18 +3,22 @@ export default {
     {
       id: 1,
       name: "Terms of Service",
+      src: "/terms",
     },
     {
       id: 2,
       name: "Privacy Policy",
+      src: "/privacy",
     },
     {
       id: 3,
       name: "Cookie Policy",
+      src: "/cookie",
     },
     {
       id: 4,
       name: "Legal",
+      src: "/legal",
     },
   ],
 };
