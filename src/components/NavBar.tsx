5af1b53dfb3ac30d510a0429d9bc95ee908417ef
Abraham Masresha <abrehammasresha890@gmail.com>
2024-11-05 23:02:14 +0300

    refactor improvments

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,31 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Avatar, Box, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
+import useNavStore from "../store/useNavStore.ts";
 
 const NavBar = () => {
+  const { isLoggedIn } = useNavStore();
   return (
-    <Box boxShadow="md" background={"blue.100"}>
+    <Box
+      as={"nav"}
+      display={"flex"}
+      boxShadow="md"
+      background={"blue.100"}
+      alignItems={"center"}
+      justifyContent={"space-between"}
+      flexDirection={"row"}
+    >
       <Box paddingX={5} paddingY={3.5} color="white">
         <Link to={"/"}>
           <Image width={"96px"} height={"26px"} src={logo} />
         </Link>
       </Box>
+
+      {isLoggedIn ? (
+        <Box paddingX={5} paddingY={3.5} color="white">
+          <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+        </Box>
+      ) : null}
     </Box>
   );
 };
